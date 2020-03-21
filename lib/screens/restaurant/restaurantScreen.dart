82a94d2236ef3d15df356f9b598da484b9e76be3
Saqib S <msaqibsaeed4@gmail.com>
2020-03-21 14:19:31 +0500

    restaurantScreenFinished

--- lib/screens/restaurant/restaurantScreen.dart
+++ lib/screens/restaurant/restaurantScreen.dart
@@ -21,7 +21,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
             FontAwesomeIcons.shoppingCart,
             color: Constants().secondaryColor,
           ),
-          onPressed: (){
+          onPressed: () {
             print('FAB Pressed');
           },
         ),
@@ -77,40 +77,95 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
           SizedBox(
             height: 20.0,
           ),
-          Image.network(
-            'https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1200px-KFC_logo.svg.png',
-            height: 50.0,
-            width: 50.0,
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            height: 50.0,
-            child: FlatButton(
-              onPressed: () {
-                print('Our Menu Pressed');
-              },
-              child: Text(
-                'Our Menu',
-                style: TextStyle(
-                  fontSize: 20.0,
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Image.network(
+                  'https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1200px-KFC_logo.svg.png',
+                  height: 50.0,
+                  width: 50.0,
                 ),
-              ),
-              color: Constants().primaryColor,
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Image.network(
-            'https://resources.stuff.co.nz/content/dam/images/1/d/n/e/j/k/image.related.StuffLandscapeSixteenByNine.710x400.1dnee9.png/1481675159935.jpg',
-          ),
-          SizedBox(
-            height: 50.0,
-            width: 120.0,
-            child: Divider(
-              color: Constants().primaryColor,
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  height: 50.0,
+                  child: FlatButton(
+                    onPressed: () {
+                      print('Our Menu Pressed');
+                    },
+                    child: Text(
+                      'Our Menu',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    color: Constants().primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Image.network(
+                  'https://resources.stuff.co.nz/content/dam/images/1/d/n/e/j/k/image.related.StuffLandscapeSixteenByNine.710x400.1dnee9.png/1481675159935.jpg',
+                ),
+                SizedBox(
+                  height: 30.0,
+                  width: 120.0,
+                  child: Divider(
+                    color: Constants().primaryColor,
+                  ),
+                ),
+                Text(
+                  'Previously Purchased',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Card(
+                  child: Column(children: <Widget>[
+                    ListTile(
+                      leading: Image.network(
+                          'https://cdn-images-fishry.azureedge.net/product/Value-Bucket-f8d68d7-kfc.png/xs',
+                          
+                          ),
+                      title: Text('Friday Smoke Bucket'),
+                      subtitle: Text(
+                          'The perfect smokey break. 5 pieces of smokey chicken wings on friday'),
+                    ),
+                    ButtonBar(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.solidStar,
+                          color: Constants().primaryColor,
+                          size: 13.0,
+                        ),
+                        Text(
+                          '4.5',
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(width: 20.0,),
+                        Text('99'),
+                        SizedBox(width: 20.0,),
+                        Container(
+                          color: Constants().primaryColor,
+                          child: FlatButton(
+                            onPressed: () {
+                              print('Add to Cart Button Pressed');
+                            },
+                            child: Text(
+                              'Add to Cart',
+                              style: TextStyle(color: Constants().secondaryColor),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ]),
+                ),
+              ],
             ),
           ),
         ],
@@ -118,7 +173,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
     );
   }
 
-  Widget _buidBottomNavigation(){
+  Widget _buidBottomNavigation() {
     return BottomAppBar(
       shape: CircularNotchedRectangle(),
       color: Constants().primaryColor,
@@ -127,21 +182,14 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
         child: Padding(
           padding: const EdgeInsets.only(left: 15.0, right: 30.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget> [
-              Text(
-                'Two Items Added To Cart'
-              ),
-
-              Text(
-                'Total 100',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15.0
-                ),
-              )
-            ] 
-          ),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Two Items Added To Cart'),
+                Text(
+                  'Total 100',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+                )
+              ]),
         ),
       ),
     );
